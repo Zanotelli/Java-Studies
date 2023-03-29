02f8121e27f708e41b3b173e287e102a4fc1cfc7
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-03-29 08:55:41 -0300

    Seção 16
    - Criação da base da camada de xadrez
    - Criação da classe de "Partida de xadrez"
    - Criação da classe de UI que imprime a partida em tela

--- Secao-16/src/application/Main.java
+++ Secao-16/src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
+        ChessMatch match = new ChessMatch();
+
+        UI.printBoard(match.getPieces());
     }
 }
